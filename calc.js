4237443b4da22e8f41aee3720f6c07abf72e6312
Gavin Ho <gavintyho@gmail.com>
2024-09-14 11:51:58 -0400

    fix

---
+++ calc.js
@@ -0,0 +1,10 @@
+var numerator = 0
+var denominator = 0
+
+//Scrape HTML by each header
+//For each header id "grade", add first number to numerator, last number to denominator
+//Calculator grade, return grade as percentage and a string grade
+//Need to find <span class="tooltip">
+
+const headers = document.querySelectorAll("span.screenreader-only")
+document.getElementById('result').innerText = headers
