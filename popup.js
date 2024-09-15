5754db82edcb527eef4c17914285edf78d9a5c12
Gavin Ho <gavintyho@gmail.com>
2024-09-14 21:36:42 -0400

    human edits

--- popup.js
+++ popup.js
@@ -11,17 +11,18 @@ document.getElementById('scrape-btn').addEventListener('click', () => {
   });
   
   function getLetterGrade(percentage) {
-    if (percentage >= 93) return 'A';
-    if (percentage >= 90) return 'A-';
-    if (percentage >= 87) return 'B+';
-    if (percentage >= 83) return 'B';
-    if (percentage >= 80) return 'B-';
-    if (percentage >= 77) return 'C+';
-    if (percentage >= 73) return 'C';
-    if (percentage >= 70) return 'C-';
-    if (percentage >= 67) return 'D+';
-    if (percentage >= 63) return 'D';
-    if (percentage >= 60) return 'D-';
+    if (percentage >= 97) {return "A+"}
+    if (percentage >= 93) {return 'A'};
+    if (percentage >= 90) {return 'A-'};
+    if (percentage >= 87) {return 'B+'};
+    if (percentage >= 83) {return 'B'};
+    if (percentage >= 80) {return 'B-'};
+    if (percentage >= 77) {return 'C+'};
+    if (percentage >= 73) {return 'C'};
+    if (percentage >= 70) {return 'C-'};
+    if (percentage >= 67) {return 'D+'};
+    if (percentage >= 63) {return 'D'};
+    if (percentage >= 60) {return 'D-'};
     return 'F';
 }
 
