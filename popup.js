dd4fad8a07b624b4b37bf33d9508269be1326881
Gavin Ho <gavintyho@gmail.com>
2024-09-18 15:19:55 -0400

    test 42

--- popup.js
+++ popup.js
@@ -57,7 +57,7 @@ document.getElementById('scrape-btn').addEventListener('click', () => {
     });
 
     // Return the total sum of grades and max values
-    return `Grade: ${((totalGrades/totalMax)*100).toFixed(2)}%`;
+    return "Grade: "+((totalGrades/totalMax)*100).toFixed(2)+"%";
 }
 
 
