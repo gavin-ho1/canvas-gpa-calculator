881bc193b62690647cb85f2afe046f9f78dfb044
Gavin Ho <gavintyho@gmail.com>
2024-09-18 14:21:16 -0400

    test 8

--- inject.js
+++ inject.js
@@ -1,18 +1,17 @@
-// Find the target element with the ID "student-grades-right-content"
-const targetElement = document.getElementById('student-grades-right-content');
+// Find the target <aside> element with the ID "right-side-wrapper"
+const asideElement = document.getElementById('right-side');
 
-if (targetElement) {
+if (asideElement) {
     // Create a new div element
     const newDiv = document.createElement('div');
 
-    // Add some content to the new div
+    // Add some content to the new div with the custom font size
     newDiv.innerHTML = `
-        <div style="border: 1px solid black; padding: 20px; margin: 10px; background-color: lightgreen;">
-            <h2>Injected HTML Content</h2>
-            <p>This div was injected before #student-grades-right-content.</p>
+        <div style="font-size: 1.2em; background-color: lightblue;">
+            <p>This div was injected at the top of <aside id="right-side-wrapper"></p>
         </div>
     `;
 
-    // Insert the new div before the target element
-    targetElement.parentNode.insertBefore(newDiv, targetElement);
+    // Insert the new div as the first child of the <aside> element
+    asideElement.insertAdjacentElement('afterbegin', newDiv);
 }
