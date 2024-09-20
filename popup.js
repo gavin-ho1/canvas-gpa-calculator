9b38ecd37dee02f5cabc6ee0f7691c200c033d1f
Gavin Ho <gavintyho@gmail.com>
2024-09-20 15:24:48 -0400

    formatting

--- popup.js
+++ popup.js
@@ -65,7 +65,7 @@ if (asideElement) {
 
     // Add some content to the new div with the custom font size
     newDiv.innerHTML = `
-        <div style="font-size: 1.2em; border-bottom: black 1px; ">
+        <div style="display: flex; flex-direction: row; align-items: center; justify-content: center; font-size: 1.2em; border-bottom: 1px solid black;">
             <p>Grade: ${input}%</p>
         </div>
     `;
