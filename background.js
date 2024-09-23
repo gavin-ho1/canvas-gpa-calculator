021617316d7128ad392d201f945f7ed9ae12804f
Gavin Ho <gavintyho@gmail.com>
2024-09-23 09:49:21 -0400

    test autoload

--- background.js
+++ background.js
@@ -1,19 +1,5 @@
-function getCurrentTab(callback) {
-  let queryOptions = { active: true, lastFocusedWindow: true };
-  chrome.tabs.query(queryOptions, ([tab]) => {
-    if (chrome.runtime.lastError)
-    console.error(chrome.runtime.lastError);
-    // `tab` will either be a `tabs.Tab` instance or `undefined`.
-    callback(tab);
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.executeScript(tabs[0].id, { file: "content.js" });
   });
-}
-
-chrome.scripting
-    .executeScript({
-      target : {tabId : getCurrentTab(), allFrames : true},
-      files : [ "content.js" ],
-    })
-    .then(() => console.log("script injected"));
-
-
-  
+});
