66cb2caa57afc50f8e249593747fe4293b3a77d4
Gavin Ho <gavintyho@gmail.com>
2024-09-24 09:13:06 -0400

    WIP ID Logger

--- background.js
+++ background.js
@@ -3,13 +3,13 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     tabURL = tabs[0].url;
     chrome.scripting.executeScript(tabs[0].id, { file: "content.js" });
-    //Recive and message from content.js
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "URLRequest") {
-    sendResponse({ response: tabs[0].url });
-  }
-});
     
   });
 });
 
+//Recive and message from content.js
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "URLRequest") {
+    sendResponse({ response: tabs[0].url });
+  }
+});
