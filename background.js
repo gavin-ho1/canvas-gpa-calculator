7c0490e6380b4daefab622415d9dbe4c8b24c2cf
Gavin Ho <gavintyho@gmail.com>
2024-09-24 12:02:11 -0400

    WIP ID Logger

--- background.js
+++ background.js
@@ -10,7 +10,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'getURL') {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const url = tabs[0].url;
-      sendResponse({ url });
+      
 
       // Create a local dictionary
       const localDictionary = {};
