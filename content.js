c0abf851bec6a1b63826f508141e1f39578cce51
Gavin Ho <gavintyho@gmail.com>
2024-09-23 22:08:59 -0400

    WIP ID Logger

--- content.js
+++ content.js
@@ -1,3 +1,8 @@
+console.log("hi")
+chrome.tabs.query({"active": true, "lastFocusedWindow": true}, function (tabs) {
+  tabURL = tabs[0].url;
+  console.log("URL from get-url.js", tabURL);
+});
 
 const courseDict = {}
 
