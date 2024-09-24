5f7004df3bb4a57ef1cf877b2e7c8f5bee5eac86
Gavin Ho <gavintyho@gmail.com>
2024-09-24 11:37:21 -0400

    WIP ID Logger

--- background.js
+++ background.js
@@ -2,7 +2,7 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     tabURL = tabs[0].url;
-    chrome.scripting.executeScript(tabs[0].id, { file: "content.js" });
+    chrome.tabs.executeScript(tabs[0].id, { file: "content.js" });
     
   });
 });
@@ -11,22 +11,8 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'getURL') {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const url = tabs[0].url;
-
-      const regex = /courses\/(\d+)\/grades/; // Matches digits after "courses/"
-      const match = regex.exec(url);
-
-      if (match) {
-        const courseID = match[1];  // The captured group (digits)
-        // Do something with the extracted course number
-      }
-      console.log(courseID)
-      sendResponse({ courseID });
+      console.log(url)
+      sendResponse({ url });
     });
-  }
-});
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'log') {
-    console.log(request.message);
   }
 });
