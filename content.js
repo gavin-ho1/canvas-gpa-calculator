3636ad70417d149b52987972b27409f93bac29a3
Gavin Ho <gavintyho@gmail.com>
2024-09-23 22:09:37 -0400

    WIP ID Logger

--- content.js
+++ content.js
@@ -72,28 +72,4 @@ if (asideElement) {
 
   
 
-  //ID Logger attempt (WIP)
-  chrome.runtime.sendMessage({ action: "message_from_content", data: "hello" }, (response) => {
-    console.log("Response from background script:", response);
-  });
-chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-  var activeTab = tabs[0];
-  var url = activeTab.url;
-
-  // Check if the URL matches a specific domain or host
-  if (url.includes(".instructure.com")) {
-    console.log("URL matches the specified domain");
-
-    var regex = /\/courses\/(\d+)\//;
-    var match = regex.exec(url);
-
-    if (match) {
-      var courseID = match[1];
-      console.log("Course ID:", courseID);
-    } else {
-      console.log("URL doesn't match the expected format.");
-    }
-  } else {
-    console.log("URL doesn't match the specified domain");
-  }
-});
+  
