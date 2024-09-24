fad4299f5be308413d7ff71773388b03c2570dd1
Gavin Ho <gavintyho@gmail.com>
2024-09-23 21:47:34 -0400

    test on storing grades

--- background.js
+++ background.js
@@ -1,29 +1,8 @@
 //Run content.js on page load
 chrome.runtime.onInstalled.addListener(() => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.executeScript(tabs[0].id, { file: "content.js" });
+    chrome.tabs.executeScript(tabs[0].id, { file: ["content.js", "IDlogger.js"] });
   });
 });
 
 
-chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-  var activeTab = tabs[0];
-  var url = activeTab.url;
-
-  // Check if the URL matches a specific domain or host
-  if (url.includes(".instructure.com")) {
-    console.log("URL matches the specified domain");
-
-    var regex = /\/courses\/(\d+)\//;
-    var match = regex.exec(url);
-
-    if (match) {
-      var courseID = match[1];
-      console.log("Course ID:", courseID);
-    } else {
-      console.log("URL doesn't match the expected format.");
-    }
-  } else {
-    console.log("URL doesn't match the specified domain");
-  }
-});
