b8b4c5a0b59ef663307d3c995f46d9c5748bcb95
Gavin Ho <gavintyho@gmail.com>
2024-09-23 21:42:47 -0400

    test on storing grades

--- background.js
+++ background.js
@@ -5,12 +5,16 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
-//pull url and courseID on page load
+
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
   var activeTab = tabs[0];
   var url = activeTab.url;
-  console.log(url)
-  var regex = /\/courses\/(\d+)\//;
+
+  // Check if the URL matches a specific domain or host
+  if (url.includes(".instructure.com")) {
+    console.log("URL matches the specified domain");
+
+    var regex = /\/courses\/(\d+)\//;
     var match = regex.exec(url);
 
     if (match) {
@@ -19,4 +23,7 @@ chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     } else {
       console.log("URL doesn't match the expected format.");
     }
+  } else {
+    console.log("URL doesn't match the specified domain");
+  }
 });
