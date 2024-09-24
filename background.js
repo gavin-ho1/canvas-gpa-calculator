bc88e3d2984691092d5ac7fb723bb758cb32ae86
Gavin Ho <gavintyho@gmail.com>
2024-09-24 13:00:29 -0400

    WIP ID Logger

--- background.js
+++ background.js
@@ -14,20 +14,22 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'getURL') {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       url = tabs[0].url;
-      const regex = /\/courses\/(\d+)\//;; // Matches digits after "courses/"
-      courseID = regex.exec(url);
+      const regex = /(\d+)/; // Matches digits after "courses/"
+      courseID = url.match(regex);
 
-      sendResponse({ url });
+      sendResponse({ courseID });
     }); 
     
     chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+
+      //Get grade of current page
       if (request.type === 'getGrade') {
           grade = request.data
           chrome.storage.sync.get('localDictionary', (result) => {
             const localDictionary = result.localDictionary || {}; // Initialize if not present
             console.log(url)
-            localDictionary[url] = {
-              data: grade
+            localDictionary[courseID] = {
+              grade: grade
             };
           
             chrome.storage.sync.set({ localDictionary }, () => {
