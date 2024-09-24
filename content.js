17ec7fbe1b9465acfcac43b3d3a4e0a15fb1cd79
Gavin Ho <gavintyho@gmail.com>
2024-09-24 09:00:28 -0400

    WIP ID Logger

--- content.js
+++ content.js
@@ -62,3 +62,11 @@ if (asideElement) {
     asideElement.insertAdjacentElement('afterbegin', newDiv);
     
   }
+
+  //Send message to background.js
+  (async () => {
+    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+    const response = await chrome.tabs.sendMessage(tab.id, {greeting: "hello"});
+    // do something with response here, not outside the function
+    console.log(response);
+  })();
