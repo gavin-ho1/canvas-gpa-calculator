0d6ff029f76391b67456b284c5235b10800a9685
Gavin Ho <gavintyho@gmail.com>
2024-09-23 21:37:26 -0400

    test on storing grades

--- background.js
+++ background.js
@@ -1,10 +1,24 @@
+//Run content.js on page load
 chrome.runtime.onInstalled.addListener(() => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.executeScript(tabs[0].id, { file: "content.js" });
   });
 });
-chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-  var activeTab = tabs[0];
-  var url = activeTab.url;
-  console.log('hi'+url);
+
+//pull url and courseID on page load
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (changeInfo.url) {
+    var url = tab.url;
+
+    // Extract the numbers using regular expression
+    var regex = /\/courses\/(\d+)\//;
+    var match = regex.exec(url);
+
+    if (match) {
+      var courseId = match[1];
+      console.log("Course ID:", courseId);
+    } else {
+      console.log("URL doesn't match the expected format.");
+    }
+  }
 });
