391e3b42fcdc206356618fa5c655aa6422c33321
Gavin Ho <gavintyho@gmail.com>
2024-09-23 21:41:38 -0400

    test on storing grades

--- content.js
+++ content.js
@@ -65,18 +65,4 @@ if (asideElement) {
     asideElement.insertAdjacentElement('afterbegin', newDiv);
   }
 
-  //pull url and courseID on page load
-chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-  var activeTab = tabs[0];
-  var url = activeTab.url;
-  console.log(url)
-  var regex = /\/courses\/(\d+)\//;
-    var match = regex.exec(url);
-
-    if (match) {
-      var courseID = match[1];
-      console.log("Course ID:", courseID);
-    } else {
-      console.log("URL doesn't match the expected format.");
-    }
-});
+  
