251af2fca348b1de76e77270475bea3a522cfdce
Gavin Ho <gavintyho@gmail.com>
2024-09-24 11:35:29 -0400

    WIP ID Logger

--- background.js
+++ background.js
@@ -19,6 +19,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         const courseID = match[1];  // The captured group (digits)
         // Do something with the extracted course number
       }
+      confirm.log(courseID)
       sendResponse({ courseID });
     });
   }
