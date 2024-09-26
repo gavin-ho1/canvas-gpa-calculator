892716c059720db2868ee53b69e9ef8e352e82b8
Gavin Ho <gavintyho@gmail.com>
2024-09-26 14:02:54 -0400

    test

--- content.js
+++ content.js
@@ -84,7 +84,7 @@ chrome.runtime.sendMessage({ type: 'getURL' }, (response) => {
     }
 
   }else{
-    const gradeSpans = document.querySelectorAll('span.grade');  // Selects all <span class="grade">
+    const gradeSpans = document.querySelectorAll('div.student_assignment final_grade');  // Selects all <span class="grade">
     gradeSpans.forEach(span => {
       const gradeText = span.innerText.trim();
 
