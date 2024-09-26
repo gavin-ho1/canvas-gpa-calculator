ba3ef6141724e29f102c0245c39831ffef28dd0a
Gavin Ho <gavintyho@gmail.com>
2024-09-26 09:26:29 -0400

    test

--- background.js
+++ background.js
@@ -22,7 +22,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       const regex = /courses\/(\d+)\/grades/; // Matches digits after "courses/"
       courseID = regex.exec(url)[1];
 
-      sendResponse({ courseID, url });
+      sendResponse([ courseID, url ]);
     }); 
     
     chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
