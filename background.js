7680df9ab9752104f60e88fd859d27f97728c1db
Gavin Ho <gavintyho@gmail.com>
2024-09-26 20:24:04 -0400

    test

--- background.js
+++ background.js
@@ -12,14 +12,34 @@ chrome.runtime.onInstalled.addListener(() => {
       chrome.tabs.executeScript(tabs[0].id, { file: "content.js" });
       
     }else {
-      console.log("running dashboard.js")
+      
       chrome.tabs.executeScript(tabs[0].id, { file: "dashboard.js" });
       
     }
   });
 });
 
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
+  //Get grade of current page
+  if (request.type === 'getGrade') {
+      grade = request.data
+      console.log(grade)
+      chrome.storage.sync.get('courseDict', (result) => {
+        const courseDict = result.courseDict || {}; // Initialize if not present
+        courseDict[courseID] = {
+          data: grade
+        };
+        console.log(courseID, grade)
+        // For debugging
+        chrome.storage.sync.set({ courseDict }, () => {
+          console.log(courseDict)
+          // For Debuging
+        });
+        
+      });
+  }
+});
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'getURL') {
