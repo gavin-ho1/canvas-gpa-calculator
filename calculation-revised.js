73b237c93634edf53f86128a3fbd8c11dcfa3b20
Gavin Ho <gavintyho@gmail.com>
2024-09-27 13:04:50 -0400

    test

--- calculation-revised.js
+++ calculation-revised.js
@@ -42,5 +42,5 @@ gradedAssigments = document.querySelectorAll("tr.student_assignment.assignment_g
 gradedAssigmentTypes = document.querySelectorAll("tr.student_assignment.assignment_graded.editable th.title div.context")
 gradedAssigmentGrades = document.querySelectorAll("tr.student_assignment.assignment_graded.editable td.assignment_score div.score_holder span.tooltip span.grade")
 gradedAssigmentGrades.forEach(grade => {
-    chrome.runtime.sendMessage({ type: 'print', data : grade.innerHTML}, (response) => {}); 
+    chrome.runtime.sendMessage({ type: 'print', data : grade.innerHTML.match('/\d+/')}, (response) => {}); 
 })
