9d810c256f69e5adb3053e0d2689f59ba9367dae
Gavin Ho <gavintyho@gmail.com>
2024-09-27 09:32:17 -0400

    test

--- content.js
+++ content.js
@@ -94,7 +94,10 @@ const gradeHeaders = document.querySelectorAll('h2')
     }
 
   }else{
-    const gradeWrappers = document.querySelectorAll("#submission_final-grade > td.assignment_score > div > span.tooltip > span")
+    
+    chrome.runtime.sendMessage({ type: 'print', data : "Auto Calculate is detected" }, (response) => {});
+
+    const gradeWrappers = document.querySelectorAll("#student-grades-right-content > div.student_assignment.final_grade > span.grade")
     gradeWrappers.forEach(function(gradeWrapper){
       text = gradeWrapper.innerHTML
       chrome.runtime.sendMessage({ type: 'print', data : text }, (response) => {});
