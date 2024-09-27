b8bd3677b2b7aac0d24c3c96f288380ec9a18701
Gavin Ho <gavintyho@gmail.com>
2024-09-27 13:05:01 -0400

    test

--- content.js
+++ content.js
@@ -9,7 +9,7 @@ chrome.runtime.sendMessage({ type: 'getURL' }, (response) => {
 gradedAssigments = document.querySelectorAll("tr.student_assignment.assignment_graded.editable th.title div.context")
 gradedAssigmentGrades = document.querySelectorAll("tr.student_assignment.assignment_graded.editable td.assignment_score div.score_holder span.tooltip span.grade")
 gradedAssigmentGrades.forEach(grade => {
-    chrome.runtime.sendMessage({ type: 'print', data : grade.innerHTML}, (response) => {}); 
+  chrome.runtime.sendMessage({ type: 'print', data : grade.innerHTML.match('/\d+/')}, (response) => {}); 
 })
 
 var weightedGradingEnabled = false
