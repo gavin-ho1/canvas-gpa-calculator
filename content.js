f12f39710a1db3c1f3cd78f27b35aa7b7e29da9f
Gavin Ho <gavintyho@gmail.com>
2024-09-26 21:46:01 -0400

    test

--- content.js
+++ content.js
@@ -86,7 +86,7 @@ chrome.runtime.sendMessage({ type: 'getURL' }, (response) => {
     }
 
   }else{
-    const gradeWrapper = document.querySelector('tr.student_assignment.hard_coded.final_grade td.assignment_score span.tooltip span.grade');  
+    const gradeWrapper = document.querySelector('tr.student_assignment.hard_coded.final_grade td.assignment_score div.score_holder span.tooltip span.grade');  
     text = gradeWrapper.innerHTML
     chrome.runtime.sendMessage({ type: 'print', data : text }, (response) => {});
      
