6289bfc03ff6903dc6195a7c9a2dcec275f5f522
Gavin Ho <gavintyho@gmail.com>
2024-09-30 16:22:18 -0400

    test

--- content.js
+++ content.js
@@ -67,7 +67,7 @@ if(weightedGradingEnabled){
   categoriesWrappers.forEach(div => {
     categoriesList.push(div.innerHTML)
   })
-  const gradedAssigmentGradeWrappers = document.querySelectorAll("th.title")
+  const gradedAssigmentGradeWrappers = document.querySelectorAll("td.assignment_score")
   
   gradedAssigmentGradeWrappers.forEach(span => {
     chrome.runtime.sendMessage({ type: 'print', data : span.innerHTML }, (response) => {}); 
