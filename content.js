efb125042f7e74fa4d9e2a8d4389a34199879836
Gavin Ho <gavintyho@gmail.com>
2024-09-30 23:03:35 -0400

    html injection

--- content.js
+++ content.js
@@ -80,7 +80,8 @@ if(weightedGradingEnabled){
   
   gradedAssigmentGradeWrappers.forEach(span => {
     // chrome.runtime.sendMessage({ type: 'print', data : span.innerHTML.trim() }, (response) => {}); 
-    num = span.innerHTML.trim().match(/\d+(\.\d+)?$/)
+    num = span.innerHTML.trim().match(/\d+(\.\d+)?$/) //Get numbers without percentage signs next to them
+    
     if(num){
       // chrome.runtime.sendMessage({ type: 'print', data : "Grade  detected" }, (response) => {}); 
       gradeList.push(parseFloat(num[0]))
