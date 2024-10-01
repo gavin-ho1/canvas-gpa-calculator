be1c526a4fc93260f8de7c129ebc55400331e61f
Gavin Ho <gavintyho@gmail.com>
2024-10-01 10:15:28 -0400

    GPA Calc

--- content.js
+++ content.js
@@ -9,7 +9,7 @@ if(dashboardSpan){
   chrome.storage.sync.get('courseDict', (result) => {
     const courseDict = result.courseDict
     Object.keys(courseDict).forEach(key => {
-      gradePoint = courseDict[key][3]
+      gradePoint = courseDict[key][2]
       chrome.runtime.sendMessage({ type: 'print', data : gradePoint }, (response) => {}); 
     })
   })
