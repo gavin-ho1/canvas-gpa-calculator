932d71a35ef51239c6f21f291cdf317d9ada5d76
Gavin Ho <gavintyho@gmail.com>
2024-10-01 14:33:20 -0400

    reformat

--- scripts/content.js
+++ scripts/content.js
@@ -19,7 +19,7 @@ if(dashboardSpan){
     //Put HTML inject here:
     titleSpan = document.querySelector("#dashboard_header_container > div > span > span:nth-child(1) > span > span")
     chrome.runtime.sendMessage({ type: 'print', data : titleSpan.textContent }, (response) => {}); 
-    titleSpan.innerHTML += " ┃ GPA: "+GPA
+    titleSpan.innerHTML += " | GPA: "+GPA
 
     
 
