c89826454e4bf33c36d6fcaa612b38850afe79c6
Gavin Ho <gavintyho@gmail.com>
2024-10-01 12:47:53 -0400

    inject test

--- scripts/content.js
+++ scripts/content.js
@@ -19,10 +19,7 @@ if(dashboardSpan){
         //Put HTML inject here:
     titleSpan = document.querySelector("#dashboard_header_container > div > span > span:nth-child(1) > span > span")
     chrome.runtime.sendMessage({ type: 'print', data : titleSpan.textContent }, (response) => {}); 
-    titleSpan.innerHTML += " GPA: "+GPA
-
-
-    document.querySelector("#dashboard-activity > div").innerHTML = document.querySelector("#dashboard-activity > div").innerHTML + `<h2 class="recent-activity-header"> Hi </h2>`
+    titleSpan.innerHTML += " | GPA: "+GPA
 
     
     //If Card View
