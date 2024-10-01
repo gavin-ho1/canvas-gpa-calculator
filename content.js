0e32f020f50b9e831a1994196fdbf1c606126983
Gavin Ho <gavintyho@gmail.com>
2024-09-30 23:02:36 -0400

    html injection

--- content.js
+++ content.js
@@ -80,7 +80,7 @@ if(weightedGradingEnabled){
   
   gradedAssigmentGradeWrappers.forEach(span => {
     // chrome.runtime.sendMessage({ type: 'print', data : span.innerHTML.trim() }, (response) => {}); 
-    num = span.innerHTML.trim().match(/^\d+(\.\d+)?$/)
+    num = span.innerHTML.trim().match(/\d+(\.\d+)?$/)
     if(num){
       // chrome.runtime.sendMessage({ type: 'print', data : "Grade  detected" }, (response) => {}); 
       gradeList.push(parseFloat(num[0]))
