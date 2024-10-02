f0ebd01129b9cb11871ac9b6baaea401767c3be8
Gavin Ho <gavintyho@gmail.com>
2024-10-01 20:11:32 -0400

    test

--- scripts/content.js
+++ scripts/content.js
@@ -244,11 +244,9 @@ if(dashboardSpan){
 
     const gradeDivs = document.querySelectorAll('#student-grades-final');
     // Loop through each of the found divs
-    var autoGrade = True
   gradeDivs.forEach(div => {
       // Check if the text content matches "Calculation of totals has been disabled"
       if (div.textContent.trim() === "Calculation of totals has been disabled") {
-        autoGrade = False
         div.remove()
           
       } 
@@ -256,7 +254,7 @@ if(dashboardSpan){
   
 
   //Find 
-  if(autoGrade){
+  if(document.querySelector("div.student_assignment.final_grade")){
     function findGradeSpan() {
       const gradeSpan = document.querySelector("div.student_assignment.final_grade");
       
