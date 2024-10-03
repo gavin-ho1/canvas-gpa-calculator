d631e62b89a7177e6664cfea2c63bf08db3d6694
Gavin Ho <gavintyho@gmail.com>
2024-10-03 16:14:26 -0400

    Better Canvas Integration

--- scripts/content.js
+++ scripts/content.js
@@ -61,7 +61,7 @@ if(dashboardSpan){
               
               if(url.match(key)){
                 chrome.runtime.sendMessage({ type: 'print', data : courseDict[key].grade }, (response) => {}); 
-                betterCanvasCards.remove()
+                card.innerHTML = courseDict[key].grade
               }
             })
           })
