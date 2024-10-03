5b39729cf75937b4bf8a74ce000206729ac69d50
Gavin Ho <gavintyho@gmail.com>
2024-10-02 20:06:01 -0400

    test

--- scripts/content.js
+++ scripts/content.js
@@ -138,11 +138,11 @@ if(dashboardSpan){
         num = span.innerHTML.trim().match(/\d+(\.\d+)?$/) //Get numbers without percentage signs next to them
     
         if(num){
-          // chrome.runtime.sendMessage({ type: 'print', data : "Grade  detected" }, (response) => {}); 
+          chrome.runtime.sendMessage({ type: 'print', data : "Grade  detected" }, (response) => {}); 
           gradeList.push(parseFloat(num[0]))
           totalPointList.push(parseFloat(span.nextElementSibling.innerHTML.replace("/","")))
         }else{
-          // chrome.runtime.sendMessage({ type: 'print', data : "Grade not detected" }, (response) => {}); 
+          chrome.runtime.sendMessage({ type: 'print', data : "Grade not detected" }, (response) => {}); 
           gradeList.push("--")
           totalPointList.push("--")
         }
