4be845cd8f3b7d5d3a8db89be5b4f901169dc02d
Gavin Ho <gavintyho@gmail.com>
2024-10-02 20:45:08 -0400

    debug

--- scripts/content.js
+++ scripts/content.js
@@ -140,7 +140,7 @@ if(dashboardSpan){
         if(num){
           // chrome.runtime.sendMessage({ type: 'print', data : "Grade  detected" }, (response) => {}); 
           gradeList.push(parseFloat(num[0]))
-          // totalPointList.push(parseFloat(span.nextElementSibling.innerHTML.replace("/","")))
+          totalPointList.push(parseFloat(span.nextElementSibling.innerHTML.replace("/","")))
           // chrome.runtime.sendMessage({ type: 'print', data :  span.nextElementSibling.innerHTML.replace("/","") }, (response) => {});  
         }else{
           // chrome.runtime.sendMessage({ type: 'print', data : "Grade not detected" }, (response) => {}); 
