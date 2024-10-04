40e725e35a3390983e0aedb1f27bd24a9eb17d79
Gavin Ho <gavintyho@gmail.com>
2024-10-03 21:41:35 -0400

    Better Canvas Integration

--- scripts/content.js
+++ scripts/content.js
@@ -109,8 +109,7 @@ if(dashboardSpan){
       activityHeader = document.querySelector("div.page_header_container")
       if(activityHeader){
         chrome.runtime.sendMessage({ type: 'print', data: "Injected Recent Activity view" }, (response) => {}); 
-        activityHeader.innerHTML = `<h2 class = "recent-activity-header">GPA: ${GPA}</h2> 
-          ` + activityHeader.innerHTML 
+        activityHeader.innerHTML = `<h2 class = "recent-activity-header">GPA: ${GPA}</h2> <span></span>` + activityHeader.innerHTML 
         activityHeader.style.fontWeight = "bold";
 
       }else{
