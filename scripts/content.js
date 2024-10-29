013c718307e29b67edf00771481e903c04c08b7f
Gavin Ho <gavintyho@gmail.com>
2024-10-29 11:17:26 -0400

    exclude ungraded assigments

--- scripts/content.js
+++ scripts/content.js
@@ -252,12 +252,12 @@ if(dashboardSpan){
         }
       
       }
-      var countedWeight = 0
+      var countedWeight = {}
       filteredKeys.forEach(category => {
         // chrome.runtime.sendMessage({ type: 'print', data : pointDict[category]/totalPointDict[category] }, (response) => {}); 
         if(totalPointDict[category] !== 0){ //Check for div by zero
           finalGradeDict[category] = pointDict[category]/totalPointDict[category]
-          countedWeight += finalGradeDict[category]
+          countedWeight.category = finalGradeDict[category] 
         }else{
           finalGradeDict[category] = "NaN"
         }
