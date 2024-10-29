36c56ca56fd3a4d695624a711d5be0b6119669d2
Gavin Ho <gavintyho@gmail.com>
2024-10-29 11:03:40 -0400

    exclude ungraded assigments

--- scripts/content.js
+++ scripts/content.js
@@ -229,6 +229,7 @@ if(dashboardSpan){
       const gradedAssigmentGradeWrappers = document.querySelectorAll("td.assignment_score span.grade")
       
       gradedAssigmentGradeWrappers.forEach(span => {
+        if (span.innerHTML.includes("Instructor has not posted this grade") === false){
         // chrome.runtime.sendMessage({ type: 'print', data : span.innerHTML.trim() }, (response) => {}); 
         num = span.innerHTML.trim().match(/\d+(\.\d+)?$/) //Get numbers without percentage signs next to them
     
@@ -242,7 +243,7 @@ if(dashboardSpan){
           totalPointList.push("--")
         }
       
-        
+        }
       })
       for(index in gradeList){
         if(gradeList[index] !== "--"){
