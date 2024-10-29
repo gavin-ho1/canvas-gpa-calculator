0d6ef7b9f8baa1f9d28f683852763d7302ab06c4
Gavin Ho <gavintyho@gmail.com>
2024-10-29 11:23:48 -0400

    exclude ungraded assigments

--- scripts/content.js
+++ scripts/content.js
@@ -278,7 +278,7 @@ if(dashboardSpan){
       
       chrome.runtime.sendMessage({ type: 'print', data : countedWeight }, (response) => {});
 
-      finalGrade /= countedWeight
+      finalGrade *= countedWeight
       finalGrade = finalGrade.toFixed(2)
       //Debug Print
   // chrome.runtime.sendMessage({ type: 'print', data : weightDict }, (response) => {});
