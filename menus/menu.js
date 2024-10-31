327b5cfebf1d52b2d0df1971e69fc90e31289a7a
Gavin Ho <gavintyho@gmail.com>
2024-10-31 11:21:53 -0400

    test

--- menus/menu.js
+++ menus/menu.js
@@ -1,7 +1,6 @@
 
-document.addEventListener('DOMContentLoaded', () => {
+
 const openUrlButton = document.getElementById("openUrlButton");
 openUrlButton.addEventListener('click', () => {
     chrome.runtime.sendMessage({ action: 'autoLoad' });
   });
-})
