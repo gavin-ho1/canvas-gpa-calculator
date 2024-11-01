14fed019af7d358eed00ed68c9890e5a04a42bd3
Gavin Ho <gavintyho@gmail.com>
2024-11-01 11:35:19 -0400

    test

--- scripts/content.js
+++ scripts/content.js
@@ -340,11 +340,16 @@ if(dashboardSpan){
     letterGrade = "F";
   }
   
-
-      if(document.querySelector("span input#grading_period_select_menu").title.includes("All")){
+  const observer = new MutationObserver(() => {
+    if (document.querySelector("span input#grading_period_select_menu")) {
+      if(document.querySelector("span input#grading_period_select_menu").title.includes("All Grading Periods")){
         chrome.runtime.sendMessage({ type: 'print', data : "All Grading Periods" }, (response) => {}); 
         chrome.runtime.sendMessage({type: "getGrade", data : [finalGrade,courseID,letterGrade]})
       }
+        observer.disconnect();
+    }
+});
+
   
 
   chrome.runtime.sendMessage({ type: 'print', data : "Final Grade: "+finalGrade+"% ("+letterGrade+")" }, (response) => {}); 
