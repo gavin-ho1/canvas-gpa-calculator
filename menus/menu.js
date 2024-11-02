fe435a49a09e180dfa070d4e089faa5d98203ccc
Gavin Ho <gavintyho@gmail.com>
2024-11-01 21:38:32 -0400

    test

--- menus/menu.js
+++ menus/menu.js
@@ -1,6 +1,6 @@
 chrome.storage.sync.get('courseList', (result) => {
   urls = result.data
-  urls = [chrome.extension.getURL("goa-scale.html")]
+  urls = [chrome.extension.getURL("gpa-scale.html")]
   document.getElementById('openUrlButton').addEventListener('click', () => {
     urls.forEach(url => {
       chrome.tabs.create({ url });
