1f5f8fab44895a89fc3a14b9ec267f8d3a0c199d
Gavin Ho <gavintyho@gmail.com>
2024-11-02 15:51:08 -0400

    test

--- scripts/content.js
+++ scripts/content.js
@@ -42,9 +42,9 @@ if(dashboardSpan){
     chrome.runtime.sendMessage({ type: 'print', data : "GPA:" }, (response) => {});
     chrome.runtime.sendMessage({ type: 'print', data : GPA }, (response) => {}); // GPA variable must be within chrome.storage.sync.get(), otherwise the variable doesn't get saved
     
-    if(GPA === "NaN"){
+    // if(GPA === "NaN"){
       GPA = "No course grade saved"
-    }
+    // }
     
     //Put HTML inject here:
 
