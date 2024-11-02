5586274df26987d3da3f54975ae6d5af60ee0371
Gavin Ho <gavintyho@gmail.com>
2024-11-01 21:39:35 -0400

    test

--- menus/menu.js
+++ menus/menu.js
@@ -1,6 +1,5 @@
 chrome.storage.sync.get('courseList', (result) => {
   urls = result.data
-  urls = ["https://www.google.com/"]
   document.getElementById('openUrlButton').addEventListener('click', () => {
     urls.forEach(url => {
       chrome.tabs.create({ url });
