0203924a47dc215512b054b3096441ceff37ccef
Gavin Ho <gavintyho@gmail.com>
2024-11-01 20:33:00 -0400

    test

--- menus/menu.js
+++ menus/menu.js
@@ -1,6 +1,8 @@
-
-
-const openUrlButton = document.getElementById("openUrlButton");
-openUrlButton.addEventListener('click', () => {
-    chrome.runtime.sendMessage({ action: 'autoLoad' });
+const urls = ['https://example1.com',
+  'https://example2.com',
+  'https://example3.com']
+document.getElementById('openUrlButton').addEventListener('click', () => {
+  urls.forEach(url => {
+    chrome.tabs.create({ url });
   });
+});
