a86056050e5b3834e585fef2bfccc73fd2651c2e
Gavin Ho <gavintyho@gmail.com>
2024-11-04 11:37:43 -0500

    test

--- scripts/background.js
+++ scripts/background.js
@@ -34,9 +34,9 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 
     tempList = []
-  //   courseObjs.forEach(course => {
+    courseObjs.forEach(course => {
   //     console.log(course.href); // Log the course element
-  // });
+  });
 
     console.log(tempList)
     chrome.storage.sync.set({ "courseLink" : tempList})
