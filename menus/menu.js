d1b782d7fc6e176f873124a3e2511f9a6a02993a
Gavin Ho <gavintyho@gmail.com>
2024-11-04 13:05:50 -0500

    test

--- menus/menu.js
+++ menus/menu.js
@@ -56,8 +56,6 @@ document.getElementById('openUrlButton').addEventListener('click', () => {
 
 
 
-
-
 document.getElementById('link').addEventListener('click', () => {
     chrome.tabs.create({ url: "https://github.com/gavin-ho1/canvas-gpa-calculator" });
 });
