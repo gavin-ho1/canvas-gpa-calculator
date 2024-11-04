6afb92c9c2e752f83038a2bb81d81ce5882ea5ad
Gavin Ho <gavintyho@gmail.com>
2024-11-04 12:14:18 -0500

    test

--- menus/menu.js
+++ menus/menu.js
@@ -1,10 +1,17 @@
 document.getElementById('openUrlButton').addEventListener('click', () => {
   chrome.storage.sync.get('courseLinks', (result) => {
     urls = result.courseLinks
-    urls.forEach(url => {
-      chrome.runtime.sendMessage({ type: 'print', data: url });
-      chrome.tabs.create({ url: url, active: false });
-    });
+      urls.forEach(url => {
+        chrome.runtime.sendMessage({ type: 'print', data: url });
+        chrome.tabs.create({ url: url, active: false });
+      });
+      chrome.tabs.onUpdated.addListener(function onUpdated(tabId, changeInfo) {
+        if (tabId === tab.id && changeInfo.status === 'complete') {
+          console.log("Closing tab after load:", tabId);
+          chrome.tabs.remove(tabId); // Close the tab
+          chrome.tabs.onUpdated.removeListener(onUpdated); // Remove the listener to avoid unnecessary checks
+        }
+
     });
 })
 
