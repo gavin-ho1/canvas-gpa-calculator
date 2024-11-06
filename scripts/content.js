b67450a715deaf7c9beb535ed2ebdb388769db31
Gavin Ho <gavintyho@gmail.com>
2024-11-05 21:42:49 -0500

    test

--- scripts/content.js
+++ scripts/content.js
@@ -345,8 +345,11 @@ if(dashboardSpan){
     var totalPoints = 0
     gradedAssigmentGradeWrappers.forEach(span => {
       num = parseFloat(span.innerHTML.match(/(\d+)/)[0])
+
+      chrome.runtime.sendMessage({ type: 'print', data : span.innerHTML.trim() }, (response) => {});
+
       points += num
-      totalPoints += parseFloat(span.nextElementSibling.innerHTML.match(/(\d+)/)[0])
+      totalPoints += parseFloat(span.nextElementSibling.innerHTML.match(/(\d+(\.\d+)?)/)[0])
       
     })
     finalGrade = (points/totalPoints)*100
